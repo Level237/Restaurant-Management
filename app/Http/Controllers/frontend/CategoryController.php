6d4323a93a634bd0a49b2e5af68bf359991fc4b5
level237 <Bramslevel129@gmail.com>
2022-04-21 15:35:39 +0200

    update

--- app/Http/Controllers/frontend/CategoryController.php
+++ app/Http/Controllers/frontend/CategoryController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
     public function index(){
 
-        return 'Category index';
+        $categories=Category::all();
+        return view('Categories.index',compact('categories'));
 
     }
 }
