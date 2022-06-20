725e0a93a28f0938f20205a40179be026b48bfb8
level237 <Bramslevel129@gmail.com>
2022-06-20 15:26:20 +0200

    Request Category

--- resources/views/admin/tables/create.blade.php
+++ resources/views/admin/tables/create.blade.php
@@ -13,11 +13,11 @@
             </div>
             <div class="m-2 p-2 bg-white rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form action="" enctype="multipart/form-data">
+                    <form action="{{ route('admin.categories.store') }}" method="POST" enctype="multipart/form-data">
                     <div class="sm:col-span-6">
-                        <label for="title" class="block text-sm font-medium text-gray-700">Name</label>
+                        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
                         <div class="mt-1">
-                            <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2">
+                            <input type="text" id="title"  name="name" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2">
                         </div>
                     </div>
                     <div class="sm:col-span-6">
@@ -28,8 +28,8 @@
                     </div>
                     <div class="sm:col-span-6">
 
-                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+                        <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                        <textarea name="description" id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
 
                     </div>
                     <div class="mt-6 p-4">
