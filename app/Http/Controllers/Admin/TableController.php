2e5c23b67f2a29efdfac77c49e116552f2594a0b
level237 <Bramslevel129@gmail.com>
2022-06-23 17:56:42 +0200

    update

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -65,9 +65,9 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Table $table)
     {
-        //
+        return view('admin.table.edit',compact('table'));
     }
 
     /**
