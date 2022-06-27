8abe879ed930ecb106075a4647e22277f4b0422b
level237 <Bramslevel129@gmail.com>
2022-06-27 17:27:53 +0200

    front-end controller:implementation

--- app/Http/Controllers/frontend/CategoryController.php
+++ app/Http/Controllers/frontend/CategoryController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+
+        return 'Category index';
+
+    }
 }
--- app/Http/Controllers/frontend/MenuController.php
+++ app/Http/Controllers/frontend/MenuController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    //
+    public function index(){
+
+        return 'menu index';
+
+    }
 }
