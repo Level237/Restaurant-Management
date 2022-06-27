40c87850429d2c1dbb39e47fd2d5a7cc60925094
level237 <Bramslevel129@gmail.com>
2022-06-27 17:20:52 +0200

    front-end route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Use App\Http\Controllers\Admin\CategoryController;
 Use App\Http\Controllers\Admin\MenuController;
 Use App\Http\Controllers\Admin\TableController;
 Use App\Http\Controllers\Admin\ReservationController;
+Use App\Http\Controllers\frontend\CategoryController as FrontendCategoryController;
+Use App\Http\Controllers\frontend\MenuController as FrontendMenuController;
+Use App\Http\Controllers\frontend\reservationController as FrontendReservationController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,6 +23,14 @@ Use App\Http\Controllers\Admin\ReservationController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categories',[FrontendCategoryController::class,'index'])->name('categories.index');
+Route::get('/categories/{category}',[FrontendCategoryController::class,'show'])->name('categories.show');
+Route::get('/menus',[FrontendMenuController::class,'index'])->name('menus.index');
+Route::get('/reservations/stepOne',[FrontendReservationController::class,'stepOne'])->name('reservation.stepOne');
+Route::get('/reservations/stepTwo',[FrontendReservationController::class,'steptwo'])->name('reservation.stepTwo');
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
