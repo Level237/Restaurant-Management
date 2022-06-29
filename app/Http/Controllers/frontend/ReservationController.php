0b73cd54464eeb349fe50f5eaad5d8a2c8b7b3d9
level237 <Bramslevel129@gmail.com>
2022-06-29 17:48:19 +0200

    update

--- app/Http/Controllers/frontend/ReservationController.php
+++ app/Http/Controllers/frontend/ReservationController.php
@@ -4,16 +4,16 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Reservations;
+use App\Models\Reservation;
 use Carbon\Carbon;
 
 class ReservationController extends Controller
 {
     public function stepOne(Request $request){
-        $reservations=$request->session()->get('reservation');
-        $min_date=Carbon::today;
+        $reservation=$request->session()->get('reservation');
+        $min_date=Carbon::today();
         $max_date=Carbon::now()->addWeek();
-        return view('reservations.step-one',compact('reservations','min_date','max_date'));
+        return view('reservations.step-one',compact('reservation','min_date','max_date'));
 
     }
 }
