2d5adf941006278070d2ad814bb69ca2f57ae47c
level237 <Bramslevel129@gmail.com>
2022-06-29 13:56:04 +0200

    update

--- resources/views/Categories/index.blade.php
+++ resources/views/Categories/index.blade.php
@@ -4,21 +4,22 @@
         <div class="grid lg:grid-cols-4 gap-y-6">
 
             <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
-            <img class="w-full h-48" src="{{ Storage::url($category->image) }}"
-              alt="Image" />
-            <div class="px-6 py-4">
-                <a href="{{ route('categories.show',$category->id) }}">
-                  <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">{{ $category->name }}</h4>
-                </a>
+                <img class="w-full h-48" src="{{ Storage::url($category->image) }}"
+                alt="Image" />
+                <div class="px-6 py-4">
+                    <a href="{{ route('categories.show',$category->id) }}">
+                    <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">{{ $category->name }}</h4>
+                    </a>
 
 
+
+                </div>
 
             </div>
-
-          </div>
 
         </div>
         @endforeach
 
       </div>
+
 </x-guest-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,9 @@
       </div>
       <div class="flex flex-col items-center mt-12 text-center">
         <span class="relative inline-flex w-full md:w-auto">
-          <a href="#_" type="button"
+          <a href="{{ route('reservation.stepOne') }}" type="button"
             class="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-6 text-white bg-green-600 rounded-full lg:w-full md:w-auto hover:bg-green-500 focus:outline-none">
-            ake your Reservation
+            Make your Reservation
           </a>
       </div>
     </div>
